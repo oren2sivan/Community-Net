c2034e655170a929b6fa0bf351002c3b0f97ff90
Oren Sivan <orensivan07@gmail.com>
2024-10-04 20:32:09 +0300

    סקיצה שרת מרובה לקוחות

---
+++ client/client_main.py
@@ -0,0 +1,54 @@
+import socket
+import threading
+
+class Client:
+
+    def __init__(self,ip):
+        self.ip=ip
+        self.client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((ip,52124))
+        print(f"connected to server")
+        self.chat_with_server()
+    
+    
+    def chat_with_server(self):
+        send_thread = threading.Thread(target=self.send_to_server)
+        recv_thread = threading.Thread(target=self.recieve_from_server)
+
+        
+        send_thread.start()
+        recv_thread.start()
+ 
+        
+
+    def send_to_server(self):
+        while True:
+            message=input("--")
+            if message=="exit":
+                print("end of convo")
+                self.client_socket.close()
+            else:
+                message=f"client: {message}"
+                self.client_socket.send(message.encode())
+
+
+    def recieve_from_server(self):
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode()
+                if not message:
+                    pass
+                if message == "exit":
+                    print("End of convo")
+                    self.client_socket.close()
+                    break
+                print(f"Server: {message}")
+            except ConnectionResetError:
+                print("Connection closed by the server.")
+                self.client_socket.close()
+                break
+
+            
+    
+
+exm=Client("10.0.0.53")
